948da2adebe6d36271c3da3f159b2b5d69909de5
ruoshui-git <ruoshuim@gmail.com>
2020-09-28 09:46:59 -0400

    feat/w5

---
+++ 05/krewes.py
@@ -0,0 +1,11 @@
+import random
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+if __name__ == "__main__":
+    print(random.choice([value for sublist in KREWES.values()
+                         for value in sublist]))
