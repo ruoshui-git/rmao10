614f4b50efcbc13550b4d72c5069b5483da56ca2
ruoshui-git <ruoshuim@gmail.com>
2020-10-11 13:50:34 -0400

    feat/06: done

---
+++ 06_05-rf/work05_redone.py
@@ -0,0 +1,21 @@
+# Team Nine2One (Julianna, Cindy, Ruoshui)
+# SoftDev
+# K06: Learnination Through Amalgamation
+# Re-factor 05 code
+# 10/02/2020
+
+KREWES = {
+   'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+   'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+   'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+if __name__ == "__main__":
+
+    import random
+
+    # Combine the krewes into one list
+    krew = (KREWES.get('orpheus')) + (KREWES.get('rex'))+ (KREWES.get('endymion'))
+
+    # Print a random element from krew
+    print(random.choice(krew))
